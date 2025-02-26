d5c9fb73ebd60ed0a1bb5717a29fde14a824794c
rizkiii161 <155000398+rizkiii161@users.noreply.github.com>
2025-02-26 19:12:20 +0700

    Update AssignmentKii.java
    membenarkan ada yang salah dikit

--- src/assignmentkii/AssignmentKii.java
+++ src/assignmentkii/AssignmentKii.java
@@ -12,14 +12,18 @@ public class AssignmentKii {
 
     
     public static void main(String[] args) {
-        Kamar pesan1 = new KamarSuite(2,"Suite",50,2,true);
+        KamarSuite pesan1 = new KamarSuite(2,"Suite",50,2,true);
+        pesan1.pesanKamar();
         pesan1.getInfoRoom();
         System.out.println("");
-        Kamar pesan2 = new KamarStandard(4,"Standard",20,4,false);
+        KamarStandard pesan2 = new KamarStandard(4,"Standard",20,4,false);
+        pesan2.pesanKamar();
         pesan2.getInfoRoom();
         System.out.println("");
-        Kamar pesan3 = new KamarKeluarga(4,"Family",20,4,2);
+        KamarKeluarga pesan3 = new KamarKeluarga(4,"Family",20,4,2);
+        pesan3.pesanKamar();
         pesan3.getInfoRoom();
+        pesan3.batalkanPesananKamar();
     }
     
 }
